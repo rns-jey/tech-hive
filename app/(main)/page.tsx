ab02387bc6562c9018b929a7c20f29a239f902e3
JM Arenas <84744855+rns-jey@users.noreply.github.com>
2024-08-31 20:49:47 +0100

    Build discuss threads area

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,8 @@
+import { Button } from "@/components/atoms/button";
+import { Separator } from "@/components/atoms/separator";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/molecules/avatar";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/organisms/card";
+
 export default function Home() {
   const posts = [
     {
@@ -42,26 +47,88 @@ export default function Home() {
     },
   ];
 
+  const threads = [
+    {
+      title: "Bug Bounties",
+    },
+    {
+      title: "What Does a -2 Reaction Mean? Is It a Bug?",
+    },
+    {
+      title: "Latest Newsletter: Javascripts, Exponentials, Aliens & Slaves",
+    },
+    {
+      title: "Using LLMs for productivity without replacing the human originality",
+    },
+    {
+      title: "Seeking Feedback: A Chrome Extension for Tracking dev.to Reactions?",
+    },
+  ];
+
   return (
-    <main className="h-full bg-slate-50 mt-14">
+    <main className="bg-slate-50 mt-14">
       <div className="max-w-[1440px] px-8 py-4 m-auto flex space-x-4">
-        <div className="w-60 p-4 max-w-xs bg-white">Tech Hive is a community of 1,980,279 amazing developers</div>
-        <div className="flex flex-col flex-1 space-y-4">
+        {/* <div className="w-60 p-4 max-w-xs bg-white">Tech Hive is a community of 1,980,279 amazing developers</div> */}
+        <div className="w-60">
+          <Card>
+            <CardHeader>
+              <CardTitle>Tech Hive is a community of 1,980,279 amazing developers</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>We&apos;re a place where coders share, stay up-to-date and grow their careers.</p>
+            </CardContent>
+            <CardFooter className="flex flex-col space-y-1">
+              <Button className="w-full">Create account</Button>
+              <Button className="w-full">Login</Button>
+            </CardFooter>
+          </Card>
+        </div>
+
+        <div className="flex flex-col flex-1 space-y-2">
           <div className="flex space-x-6">
             <p>Relevant</p>
             <p>Latest</p>
             <p>Top</p>
           </div>
-          {posts.map((post, index) => (
-            <div key={index} className="p-4 bg-white">
-              <p>
-                {post.author} {post.date} {post.title} {post.tags.join(" ")} {post.reactions} reactions Add Comment{" "}
-                {post.readTime}
-              </p>
-            </div>
+          {posts.map((post, i) => (
+            <Card key={i}>
+              <CardHeader>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+                <CardDescription className="flex flex-col">{post.author}</CardDescription>
+                <CardDescription className="flex flex-col">{post.date}</CardDescription>
+                <CardTitle>{post.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p>{post.tags.join(" ")}</p>
+              </CardContent>
+              <CardFooter className="w-full flex gap-1">
+                <Button>{post.reactions} reactions</Button>
+                <Button>Add Comment</Button>
+                <p className="ml-auto">{post.readTime}</p>
+              </CardFooter>
+            </Card>
           ))}
         </div>
-        <div className="max-w-xs p-4 bg-white">What&apos;s happening this week</div>
+
+        <div className="w-72">
+          <Card>
+            <CardHeader>
+              <CardTitle>#discuss</CardTitle>
+              <CardDescription>Discussion threads targeting the whole community</CardDescription>
+            </CardHeader>
+            <CardContent>
+              {threads.map((thread, i) => (
+                <div key={i}>
+                  <Separator className="mb-4" />
+                  <p className="mb-4">{thread.title}</p>
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </main>
   );
